bea16711b899172eda722bef2614069fd8caf528
Caymus Price <caymus_price@yahoo.com>
2026-02-21 13:11:14 -0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/webpack-modular/',
+    base: '/modular/',
     server: {
         port: 3000,
     },
